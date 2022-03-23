71c98758d5e0c111d58944d69c1a195bce3f8118
Abu Taher Muhammad <abut1081@gmail.com>
2022-03-23 08:02:47 +0600

    Middleware test

--- frontend/pages/_middleware.js
+++ frontend/pages/_middleware.js
@@ -1,9 +1,32 @@
-export const middleware = async (req, ev) => {
-    return new Response({
+import { NextResponse } from "next/server";
+
+export const middleware = async (request, ev) => {
+    // create an instance of the class to access the public methods. This uses `next()`,
+    // you could use `redirect()` or `rewrite()` as well
+    let response = NextResponse.next();
+    // get the cookies from the request
+    let cookieFromRequest = request.cookies["my-cookie"];
+    // set the `cookie`
+    response.cookie("hello", "world");
+    // set the `cookie` with options
+    const cookieWithOptions = response.cookie("hello", "world", {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+        sameSite: "strict",
+        domain: "example.com",
+    });
+    // clear the `cookie`
+    response.clearCookie("hello");
+
+    console.log({
         ip: req.ip,
         geo: req.geo, // this will spin the globe!
         ua: req.ua,
     });
+
+    return response;
+    // return NextResponse.next();
 };
 
 export default middleware;
